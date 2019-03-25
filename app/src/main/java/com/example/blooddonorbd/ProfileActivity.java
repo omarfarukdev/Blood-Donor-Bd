7e7252fb8e38c842fea2074cab4ee1fce083e70f
FarukOmar <omar.faruq1998V@gmail.com>
2019-03-25 22:08:47 +0600

    add library function

--- app/src/main/java/com/example/blooddonorbd/ProfileActivity.java
+++ app/src/main/java/com/example/blooddonorbd/ProfileActivity.java
@@ -4,19 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class DiscoverableActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_discoverable);
-
+        setContentView(R.layout.activity_profile);
     }
 
-    public void discoverable(View view) {
-        Intent intent = new Intent(this,ProfileActivity.class);
+    public void edit(View view) {
+        Intent intent = new Intent(this,EditProfileActivity.class);
         startActivity(intent);
     }
 }
