76e3fc69bdd50e8655c68121207743108d37f5ce
FarukOmar <omar.faruq1998V@gmail.com>
2019-04-12 01:02:01 +0600

    add result details activity

---
+++ app/src/main/java/com/example/blooddonorbd/ResultDetails.java
@@ -0,0 +1,20 @@
+package com.example.blooddonorbd;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ResultDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_details);
+    }
+
+    public void messagesend(View view) {
+        Intent intent=new Intent(this,MessagesActivity.class);
+        startActivity(intent);
+    }
+}
