94c65ca60847c0c3d72a77a61aa245305bc490e7
FarukOmar <omar.faruq1998V@gmail.com>
2019-04-12 00:58:37 +0600

    hide actionbar

--- app/src/main/java/com/example/blooddonorbd/HomeActivity.java
+++ app/src/main/java/com/example/blooddonorbd/HomeActivity.java
@@ -11,6 +11,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        getSupportActionBar().hide();
     }
 
     public void search(View view) {
