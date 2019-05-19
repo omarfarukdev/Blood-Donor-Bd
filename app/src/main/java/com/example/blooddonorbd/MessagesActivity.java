fcfa0adc579cd55a3edb7ab1391c752c675cc433
FarukOmar <omar.faruq1998V@gmail.com>
2019-05-19 23:52:33 +0600

    add method and hide actionbar

--- app/src/main/java/com/example/blooddonorbd/MessagesActivity.java
+++ app/src/main/java/com/example/blooddonorbd/MessagesActivity.java
@@ -2,6 +2,7 @@ package com.example.blooddonorbd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MessagesActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MessagesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
+        getSupportActionBar().hide();
+    }
+
+    public void backBt(View view) {
+        finish();
     }
 }
